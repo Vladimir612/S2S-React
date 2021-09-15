cb4ae2ecd34fa731b1098a37cb96104e0a3ad75c
Nikola Mirilo <nikolamirilo@gmail.com>
2021-09-15 19:47:03 +0200

    created footer componet and worked on responsive cards design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import "./Footer.css";
 
-const Footer = () => {
-  return <div className="footer"></div>;
+const Footer = ({
+  facebookURL,
+  instagramURL,
+  twitterURL,
+  githubURL,
+  phoneNumber,
+  address,
+  cityAndState,
+  aboutCompany,
+  email,
+  siteName,
+}) => {
+  return (
+    <div className="footer">
+      <footer className="footer-distributed">
+        <div className="footer-left">
+          <h3>{siteName}</h3>
+
+          <p className="footer-company-name">nba-info &copy; 2021</p>
+        </div>
+
+        <div className="footer-center">
+          <div>
+            <i className="fa fa-map-marker"></i>
+            <p>
+              <span>{address}</span>
+              {cityAndState}
+            </p>
+          </div>
+
+          <div>
+            <i className="fa fa-phone"></i>
+            <p>{phoneNumber}</p>
+          </div>
+
+          <div>
+            <i className="fa fa-envelope"></i>
+            <p>
+              <a href="mailto:support@company.com">{email}</a>
+            </p>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <p className="footer-company-about">
+            <span>About the company</span>
+            {aboutCompany}
+          </p>
+
+          <div className="footer-icons">
+            <a href={facebookURL}>
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a href={instagramURL}>
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href={twitterURL}>
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a href={githubURL}>
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import Hero from './../Components/Hero/Hero';
-import Navbar from './../Components/Navbar/Navbar';
+
+//components
+import Hero from "./../Components/Hero/Hero";
+import Navbar from "./../Components/Navbar/Navbar";
+import Card from "./../Components/Card/Card";
+import Footer from "./../Components/Footer/Footer";
+
+//assets
+import img1 from "../assets/images/bg-basketball.png";
+import "../Components/Card/Card.css";
 
 const Home = () => {
   return (
@@ -10,6 +18,49 @@ const Home = () => {
       {/* <Slider />
       <Players />
       <Teams /> */}
+
+      {/* Card Experiment */}
+      <div className="card-container">
+        <Card
+          title="Lebron James"
+          text="Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor"
+          image={img1}
+          readNumber="4000"
+          viewNumber="500"
+          commentNumber="10"
+          color="rgb(173,83,59)"
+        />
+        <Card
+          title="Derick Rose"
+          text="Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor"
+          image={img1}
+          readNumber="4000"
+          viewNumber="500"
+          commentNumber="10"
+          color="rgb(173,83,59)"
+        />
+        <Card
+          title="Stephen Curry"
+          text="Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor"
+          image={img1}
+          readNumber="4000"
+          viewNumber="500"
+          commentNumber="10"
+          color="rgb(173,83,59)"
+        />
+      </div>
+      <Footer
+        siteName="NBA Info"
+        email="nbainfo@gmail.com"
+        phoneNumber="+371 2837019"
+        address="Jove Ilica 15"
+        cityAndState="Belgrade, Serbia"
+        aboutCompany="NBA Info is a website that provides informations and insights to NBA league. It is a free website for NBA lovers."
+        facebookURL="http://www.facebook.com"
+        instagramURL="https://www.instagram.com"
+        twitterURL="http://twitter.com"
+        githubURL="http://github.com"
+      />
     </div>
   );
 };
